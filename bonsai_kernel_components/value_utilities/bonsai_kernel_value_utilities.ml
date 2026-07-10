51347bc8121aa8f07f751ed77e351a3d923ef20e
public-release <as-public-release@janestreet.com>
2026-07-10 17:38:58 +0100

    v0.18~preview.130.106+341

--- bonsai_kernel_components/value_utilities/bonsai_kernel_value_utilities.ml
+++ bonsai_kernel_components/value_utilities/bonsai_kernel_value_utilities.ml
@@ -16,6 +16,48 @@ let value_with_override ?sexp_of_model ?equal value (local_ graph) =
   value, setter
 ;;
 
+let value_with_state_machine_override
+  (type model action)
+  ~(here : [%call_pos])
+  ?reset
+  ?sexp_of_model
+  ?sexp_of_action
+  ?equal
+  ~(default_model : model Bonsai.t)
+  ~(apply_action : _ -> model -> action -> model)
+  (graph @ local)
+  : model Bonsai.t * (action -> unit Bonsai.Effect.t) Bonsai.t
+  =
+  let state, inject =
+    Bonsai.state_machine_with_input
+      ~here
+      ?reset:
+        (Option.map reset ~f:(fun reset ctx model ->
+           match model with
+           | None -> None
+           | Some model -> Some (reset ctx model)))
+      ?sexp_of_model:(Option.map sexp_of_model ~f:Option.sexp_of_t)
+      ?sexp_of_action
+      ?equal:(Option.map equal ~f:Option.equal)
+      ~default_model:None
+      ~apply_action:(fun ctx default_model model action ->
+        match model with
+        | None ->
+          (match default_model with
+           | Inactive -> model
+           | Active default_model -> Some (apply_action ctx default_model action))
+        | Some model -> Some (apply_action ctx model action))
+      default_model
+      graph
+  in
+  let state =
+    match%sub state with
+    | None -> default_model
+    | Some model -> model
+  in
+  state, inject
+;;
+
 let dynamic_cutoff a ~equal (local_ graph) =
   Bonsai.both a equal
   |> Bonsai.cutoff ~equal:(fun (a, _old_equal) (b, equal) -> equal a b)
--- bonsai_kernel_components/value_utilities/bonsai_kernel_value_utilities.mli
+++ bonsai_kernel_components/value_utilities/bonsai_kernel_value_utilities.mli
@@ -7,14 +7,29 @@ module Effect := Bonsai.Effect
     the returned overriding effect is dispatched at least once, the computation will
     evaluate to the override value provided. The effect can be scheduled more than once to
     update the override. Use with [Bonsai.with_model_resetter_n] in order to revert the
-    override (see the corresponding test in test/of_bonsai_itself/test_cont_bonsai.ml for
-    an example). *)
+    override (see the corresponding test in test/test_cont.ml for an example). *)
 val value_with_override
   :  ?sexp_of_model:('a -> Sexp.t)
   -> ?equal:('a -> 'a -> bool)
   -> 'a Bonsai.t
   -> local_ Bonsai.graph
   -> 'a Bonsai.t * ('a -> unit Effect.t) Bonsai.t
+
+(** Extends a [Bonsai.state_machine] (value/setter pair) with a dynamic "default model".
+    The value will initially evaluate to the [default_model], but once [apply_action] is
+    dispatched at least once when [default_model] is active, it will reflect the updated
+    model. *)
+val value_with_state_machine_override
+  :  here:[%call_pos]
+  -> ?reset:(('action, unit) Bonsai.Apply_action_context.t -> 'model -> 'model)
+  -> ?sexp_of_model:('model -> Sexp.t)
+  -> ?sexp_of_action:('action -> Sexp.t)
+  -> ?equal:('model -> 'model -> bool)
+  -> default_model:'model Bonsai.t
+  -> apply_action:
+       (('action, unit) Bonsai.Apply_action_context.t -> 'model -> 'action -> 'model)
+  -> Bonsai.graph @ local
+  -> 'model Bonsai.t * ('action -> unit Effect.t) Bonsai.t
 
 (** Like [Bonsai.cutoff] but where the [equal] function can be determined dynamically. *)
 val dynamic_cutoff
